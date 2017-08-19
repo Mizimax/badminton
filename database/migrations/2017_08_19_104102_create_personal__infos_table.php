e6d4b082268bf8975e0bd8e3a292a342769923da
mizimax <maxsayr5@gmail.com>
2017-08-19 18:41:15 +0700

    add personal info model

---
+++ database/migrations/2017_08_19_104102_create_personal__infos_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePersonalInfosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personal__infos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('Firstname');
+            $table->string('Lastname');
+            $table->string('Nickname');
+            $table->date('Birthday');
+            $table->string('Tel'); //not sure
+            $table->integer('Rank',1,10); //not sure
+            $table->string('Team');
+            $table->string('Picture'); //should have default
+            $table->integer('Gift_Points')->default(0);
+            $table->boolean('Is_Player');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personal__infos');
+    }
+}

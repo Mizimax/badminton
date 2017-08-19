99e0dfe9f8558d0b3cab9e912efefcfe3f44ef68
mizimax <maxsayr5@gmail.com>
2017-08-19 19:04:03 +0700

    add Event Table model

---
+++ database/migrations/2017_08_19_114533_create_event_tables_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventTablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('event_tables', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('Event_Name');
+            $table->date('Event_Start');
+            $table->date('Event_End');
+            $table->integer('Rank_Min',1,10)->default(1);
+            $table->integer('Rank_Max',1,10)->default(10);
+            $table->integer('Event_Creator_Id');
+            $table->text('Event_Description');
+            $table->string('Event_Cover_Pic');
+            $table->string('Event_Image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('event_tables');
+    }
+}

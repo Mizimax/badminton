db5fcfc26de6435b9164e4114a12df88ef4ad2a2
mizimax <maxsayr5@gmail.com>
2017-08-26 02:57:35 +0700

    add event detail

--- resources/views/event/index.blade.php
+++ resources/views/event/index.blade.php
@@ -2,9 +2,46 @@
 @section('title', 'Event')
 @section('style')
 <style>
-  .padding0{
-    padding: 0 !important;
+  .padding0 {
+      padding: 0 !important;
   }
+  .event-display {
+      position: absolute !important;
+      right: auto !important;
+      bottom: auto !important;
+      top:-100px !important;
+      left:50% !important;
+      transform:translateX(-50%) !important;
+  }
+  .date{
+    color:#ccc;
+    margin-bottom:20px;
+  }
+  .dateToNow{
+    color:#888;
+    margin-bottom:15px;
+  }
+  .day{
+    color:white;
+    font-size:18px;
+  }
+  .name{
+    color: #ddd;
+    font-size:28px;
+  }
+  .head-space{
+      height:200px;
+  }
+  .icon{
+    margin-left:15px;
+    margin-right:15px;
+  }
+  .profile{
+    border-radius: 50%;
+    width:42px;
+    height: 42px;
+  }
+
 </style>
 @endsection
 @section('content')
@@ -25,7 +62,7 @@
   </div>
 @else
     <div class="column right floated four wide padding0 text-center">
-      <a href="#login" class="login-window"><img src="ICONWEBSITE KMUTTOPEN\Kmutt web prototype2-27.png " height="58" width="42"/></a>
+      <img class="profile" src="/images/guest.jpg">
       <a href="#regis" class="login-window ui blue label">Competitor</a><br>
       <form id="logout-form" action="logout" method="POST">
         {{ csrf_field() }}
@@ -39,24 +76,49 @@
 @endif
 </div>
 
+<div class="head-space">
+
+</div>
+
 <section id="dg-container" class="dg-container">
 	<div class="dg-wrapper">
-		@foreach($events as $event)
-			<a href="event/{{ $event->Event_key }}">
-				<center><img src="images/mainpic.png" alt="image01" style="width:55%;"></center>
-				<div></div>
+    @foreach($events as $event)
+      <a href="event/{{ $event->Event_key }}">
+        <div class="event-display text-center">
+          <div class="date"><span class="icon">◄</span> {{ $event->Event_Start }} <span class="icon">►</span></div>
+          <div class="dateToNow" date="{{ $event->Event_Start }}"></div>
+          <div class="name">{{ $event->Event_Name }}</div>
+        </div>
+        <script type="text/javascript">
+          var diffDays1=function(){ 
+              var ele = document.querySelector('.dateToNow');
+              var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
+              var secondDate = new Date();
+              var firstDate = new Date(ele.getAttribute("date"));
+              var days = Math.round((firstDate.getTime() - secondDate.getTime())/(oneDay));
+              if(days > 0)
+                ele.innerHTML = 'เหลือเวลาสมัครอีก<br><span class="day">' + days + 'ว ัน</span>'
+              // else if(days = 0)
+              //   ele.innerHTML = 'เหลือเวลาสมัคร'
+              else
+                ele.innerHTML = '<span class="day">หมดเวลาสมัครแล้ว</span>'
+              
+          };
+          diffDays1();
+        </script>
+				<center><img src="images/mainpic.png" alt="image01" style="width:50%;"></center>
 			</a>
 		@endforeach
 		<a href="#">
-			<center><img src="images/mainpic.png" alt="image01" style="width:55%;"></center>
+			<center><img src="images/mainpic.png" alt="image01" style="width:50%;"></center>
 			<div></div>
 		</a>
 		<a href="#">
-			<center><img src="images/mainpic.png" alt="image01" style="width:55%;"></center>
+			<center><img src="images/mainpic.png" alt="image01" style="width:50%;"></center>
 			<div></div>
 		</a>
 		<a href="#">
-			<center><img src="images/mainpic.png" alt="image01" style="width:55%;"></center>
+			<center><img src="images/mainpic.png" alt="image01" style="width:50%;"></center>
 			<div></div>
 		</a>
 			
@@ -67,7 +129,7 @@
 	</nav>
 
 
-			<form method="get" action="/search" id="searchbox5" style="margin-top: 100px;text-align:center">
+			<form method="get" action="/search" id="searchbox5" style="margin-top: 70px;text-align:center">
 				<input id="search52" name="q" type="text" size="40" placeholder="ค้นหารายการ" />
 			</form>
 
@@ -80,7 +142,7 @@
 @section('script')
 	<script type="text/javascript" src="js/modernizr.custom.53451.js"></script>
 	<script type="text/javascript" src="js/jquery.gallery.js"></script>
-	<script type="text/javascript">
+  <script type="text/javascript">
 		$(function() {
 			$('#dg-container').gallery();
 		});

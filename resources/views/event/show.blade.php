da9dd0867efc46b393aebc9e17e173509d1dd44f
Punyapat <Punyapat@admins-MacBook-Pro.local>
2017-08-31 17:36:55 +0700

    no message

--- resources/views/event/show.blade.php
+++ resources/views/event/show.blade.php
@@ -331,7 +331,7 @@ img.btn_close {
     <a href="#eventRegis" class="login-window"><button style="margin-top:10px;" class="ui violet button huge"  id="register_button"> สมัครลงเเข่ง</button></a>
     @endif
 </div>     
-< <div class="Activity_images">
+<div class="Activity_images">
 <img id="image_1" src="/images/1.png" width="31%">
 <img id="image_2" src="/images/2.png" width="29%"> 
 <img id="image_3" src="/images/3.png" width="27%">   

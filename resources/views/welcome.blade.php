9df47aa58855353748557653272ef1f03350e777
punyapat <punyapat@punyapats-MacBook-Pro.local>
2017-10-19 13:15:44 +0700

    no message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Wezync
                 </div>
 
                 <div class="links">

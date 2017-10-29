5212c0b2862422feea2ce619fa8313dab8567e64
BumBimz Ks <l3uml3imz@gmail.com>
2017-10-29 23:55:58 +0700

    migration database

---
+++ database/migration.sql
@@ -0,0 +1,15 @@
+update  `team`
+set team_race = 30
+where team_race = 7;
+
+update  `team`
+set team_race = 21
+where team_race = 6;
+
+update  `team`
+set team_race = 23
+where team_race = 8;
+
+update  `team`
+set team_race = 24
+where team_race = 9;

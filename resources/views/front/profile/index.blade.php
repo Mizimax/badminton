6464ef2f7d7eeea44f848e9fedc0750a3c2af83d
punyapat <punyapat@punyapats-MacBook-Pro.local>
2018-06-01 15:39:30 +0700

    profile up

--- resources/views/front/profile/index.blade.php
+++ resources/views/front/profile/index.blade.php
@@ -1,12 +1,63 @@
 @extends('layouts.app')
 @section('content')
 <link media="all" type="text/css" rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
-<link href="/css/profile.css" rel="stylesheet">
 <link href="/css/datepicker.min.css" rel="stylesheet">
+<link href="/css/app.css" rel="stylesheet">
+<link href="/css/profile.css" rel="stylesheet">
+
+
+
+<div class="container">
+  <div class="container-profile">
+      <div class="row">
+        <div class="col-xl-12">
+          <span class="profile-text">PLAYER PROFILE</span><br>
+          </div>
+          </div>
+        </div>
+        </div>
+
+
+      <div class="container">
+    <div class="container-profile">
+   <div class="row">
+     <div class="col-md-5">
+      <div class="box-profile-list box-profile">
+      <span>PLAYER PROFILE</span><br>
+      <span>คู่กันมากที่สุดกับ</span><br>
+      <span>kuy  kuy</span><br>
+<span>ARIKA YOKO</span><br>
+
+<div class="img-thumbnail img-circle">
+  <div style="position: relative; padding: 0; cursor: pointer;" type="file">
+    <img class="img-circle" src="/images/no_pic.jpg" style="width: 140px; height: 140px;" >
+    <span style="position: absolute; color: red; bottom: 20px; left: 40px;">UPLOAD</span>
+  </div>
+</div>
+<span>Ranking:</span><span>C+</span><br>
+
+
+
+      </div>
+  </div>
 
 
 
 
+       <div class="col-md-5">
+         <div class="box-profile-detal box-profile box-profile-list-histor">
+           <span>COMPETITION HISTOY</span><br>
+         <div class="img-thumbnail img-circle-R">
+           <div style="position: relative; padding: 0; cursor: pointer;" type="file">
+             <img class="img-circle-R" src="/images/no_pic.jpg" style="width:40px; height:40px;" >
+           </div>
+         </div>
+         <span>KMUTT OPEN</span><br>
+         </div>
+      </div>
+      </div>
+    </div>
+  </div>
 
 
 

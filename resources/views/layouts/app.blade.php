8bea04f6fb05c5e176cbc1572d5efea95ce86762
Max ang <maxsayr5@gmail.com>
2018-06-08 12:27:58 +0700

    change v

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
 
     <!-- Styles -->
     <link href="/css/app.css?v=3" rel="stylesheet">
-    <link href="/css/wezync.css?v=5.2" rel="stylesheet">
+    <link href="/css/wezync.css?v=5.3" rel="stylesheet">
     @yield('css')
 
 </head>

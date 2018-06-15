9d80549094f725b41d56baa64dfd0d3667c7bd07
Max ang <maxsayr5@gmail.com>
2018-06-16 01:23:46 +0700

    overflow

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
 
     <!-- Styles -->
     <link href="/css/app.css?v=3" rel="stylesheet">
-    <link href="/css/wezync.css?v=5.6" rel="stylesheet">
+    <link href="/css/wezync.css?v=6" rel="stylesheet">
     @yield('css')
 
 </head>

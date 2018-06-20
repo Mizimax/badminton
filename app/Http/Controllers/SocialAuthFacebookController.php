4840dc23a607d6ece3078fb160504d5cffffd3e0
Tammanoon Jomjaturong <maxsayr5@gmail.com>
2018-06-20 17:32:02 +0700

    Update SocialAuthFacebookController.php

--- app/Http/Controllers/SocialAuthFacebookController.php
+++ app/Http/Controllers/SocialAuthFacebookController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Socialite;
-use App\Services\SocialFacebookAccountService;
 
 class SocialAuthFacebookController extends Controller
 {

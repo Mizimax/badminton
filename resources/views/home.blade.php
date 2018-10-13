41377577403222aa03f71a31150af2b9e8d4ad7f
Punyapat <punyapat.areerob@mail.kmutt.ac.th>
2018-10-13 17:26:33 +0700

    add google analytics pang home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -170,13 +170,17 @@
             });
         });
     </script>
-    <script type='text/javascript'>
-    window.__lo_site_id = 114394;
+  
 
-    	(function() {
-    		var wa = document.createElement('script'); wa.type = 'text/javascript'; wa.async = true;
-    		wa.src = 'https://d10lpsik1i8c69.cloudfront.net/w.js';
-    		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(wa, s);
-    	  })();
-    	</script>
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-127412532-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-127412532-1');
+</script>
+
+
 @endsection

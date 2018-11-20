ceef048063e9f3d36cccbbd90b7d871eb5b7bca7
Punyapat <punyapat.areerob@mail.kmutt.ac.th>
2018-11-20 15:55:07 +0700

    no message

--- resources/views/front/event/dev.blade.php
+++ resources/views/front/event/dev.blade.php
@@ -3,11 +3,12 @@
 <link media="all" type="text/css" rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
 <link href="{{ asset('css/coin_shop.css') }}" rel="stylesheet">
 <div class="row">
-    <div class="body-content" style="width: 95%;" align="center">
-
+    <div class="body-content col-md-6" style="width: 75%;" align="center">
+<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd7XulB9Qnl4-khTUUzi2zmulTvaLvUdqeKm5jA9uB3xq9XSw/viewform" width="90%" height="700px;" scrolling="yes"></iframe>
     </div>
+
     <div class="col-md-1">
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd7XulB9Qnl4-khTUUzi2zmulTvaLvUdqeKm5jA9uB3xq9XSw/viewform" width="100%" height="700px;" scrolling="yes"></iframe>
+
     </div>
 </div>
 <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
